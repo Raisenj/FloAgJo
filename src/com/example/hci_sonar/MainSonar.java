f468ff4e38512dd9e3db376e9bd378362a21c851
Jonatan <jonathan.mv.87@gmail.com>
2014-06-04 02:32:51 +0200

    Recording with .wav

--- src/com/example/hci_sonar/MainSonar.java
+++ src/com/example/hci_sonar/MainSonar.java
@@ -1,10 +1,16 @@
 package com.example.hci_sonar;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,9 +21,16 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainSonar extends Activity {
+
+	private static final int RECORDER_BPP = 16;
+	private static final String AUDIO_RECORDER_FILE_EXT_WAV = ".wav";
+	private static final String AUDIO_RECORDER_TEMP_FILE = "record_temp.raw";
+	private static final int RECORDER_SAMPLERATE = 44100;
+	private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+	private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+	private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
 
 	private static final int RECORDING_TIME = 10;
 	private static final int SANCTION_TIME = 10;
@@ -27,7 +40,7 @@ public class MainSonar extends Activity {
 	private static final int SANCTIONED = 97;
 	private static final String AUDIOFILENAME = Environment
 			.getExternalStorageDirectory().getAbsolutePath()
-			+ "/audiorecordtest.mp4";
+			+ "/audiorecordtest";
 
 	private final Handler guiHandler = new Handler();
 
@@ -44,6 +57,9 @@ public class MainSonar extends Activity {
 	private int seconds_sanction;
 
 	private MediaRecorder mRecorder = null;
+	private AudioRecord recorder;
+	private int bufferSize = 0;
+	private Thread recordingThread = null;
 
 	private Runnable changeGUIToSanction = new Runnable() {
 
@@ -118,6 +134,133 @@ public class MainSonar extends Activity {
 		initGuiElements();
 	}
 
+	protected void stopRecordingAudio2() {
+
+		Log.d("FILTERED", "is recording false");
+		if (null != recorder) {
+			Log.d("FILTERED", "is recording false");
+
+			int i = recorder.getState();
+			if (i == 1)
+				recorder.stop();
+			recorder.release();
+
+			recorder = null;
+			recordingThread = null;
+		}
+
+		copyWaveFile(getTempFilename(), getFilename());
+		deleteTempFile();
+	}
+
+	private String getFilename() {
+		String filepath = Environment.getExternalStorageDirectory().getPath();
+		File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+
+		if (!file.exists()) {
+			file.mkdirs();
+		}
+
+		// return (file.getAbsolutePath() + "/" + System.currentTimeMillis() +
+		// AUDIO_RECORDER_FILE_EXT_WAV);
+		return (file.getAbsolutePath() + "/" + "Output" + AUDIO_RECORDER_FILE_EXT_WAV);
+	}
+
+	private void deleteTempFile() {
+		File file = new File(getTempFilename());
+
+		file.delete();
+	}
+
+	private void copyWaveFile(String inFilename, String outFilename) {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		long totalAudioLen = 0;
+		long totalDataLen = totalAudioLen + 36;
+		long longSampleRate = RECORDER_SAMPLERATE;
+		int channels = 2;
+		long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels / 8;
+
+		byte[] data = new byte[bufferSize];
+
+		try {
+			in = new FileInputStream(inFilename);
+			out = new FileOutputStream(outFilename);
+			totalAudioLen = in.getChannel().size();
+			totalDataLen = totalAudioLen + 36;
+
+			// AppLog.logString("File size: " + totalDataLen);
+
+			WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+					longSampleRate, channels, byteRate);
+
+			while (in.read(data) != -1) {
+				out.write(data);
+			}
+
+			in.close();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+			long totalDataLen, long longSampleRate, int channels, long byteRate)
+			throws IOException {
+
+		byte[] header = new byte[44];
+
+		header[0] = 'R'; // RIFF/WAVE header
+		header[1] = 'I';
+		header[2] = 'F';
+		header[3] = 'F';
+		header[4] = (byte) (totalDataLen & 0xff);
+		header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+		header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+		header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+		header[8] = 'W';
+		header[9] = 'A';
+		header[10] = 'V';
+		header[11] = 'E';
+		header[12] = 'f'; // 'fmt ' chunk
+		header[13] = 'm';
+		header[14] = 't';
+		header[15] = ' ';
+		header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+		header[17] = 0;
+		header[18] = 0;
+		header[19] = 0;
+		header[20] = 1; // format = 1
+		header[21] = 0;
+		header[22] = (byte) channels;
+		header[23] = 0;
+		header[24] = (byte) (longSampleRate & 0xff);
+		header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+		header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+		header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+		header[28] = (byte) (byteRate & 0xff);
+		header[29] = (byte) ((byteRate >> 8) & 0xff);
+		header[30] = (byte) ((byteRate >> 16) & 0xff);
+		header[31] = (byte) ((byteRate >> 24) & 0xff);
+		header[32] = (byte) (2 * 16 / 8); // block align
+		header[33] = 0;
+		header[34] = RECORDER_BPP; // bits per sample
+		header[35] = 0;
+		header[36] = 'd';
+		header[37] = 'a';
+		header[38] = 't';
+		header[39] = 'a';
+		header[40] = (byte) (totalAudioLen & 0xff);
+		header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+		header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+		header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+		out.write(header, 0, 44);
+	}
+
 	protected void stopRecording(boolean fromThread) {
 		// TODO Auto-generated method stub
 		STATE = SANCTIONED;
@@ -133,15 +276,8 @@ public class MainSonar extends Activity {
 		this.timer_recording = null;
 		this.timer_sanction = new Timer(true);
 		this.timer_sanction.schedule(new SanctionCounterTask(), 1000, 1000);
-		stopRecordingAudio();
-	}
-
-	private void stopRecordingAudio() {
-		// TODO Auto-generated method stub
-		this.mRecorder.stop();
-		this.mRecorder.release();
-		this.mRecorder = null;
-
+		// stopRecordingAudio();
+		stopRecordingAudio2();
 	}
 
 	private void updateGUISanctioned() {
@@ -190,43 +326,93 @@ public class MainSonar extends Activity {
 		this.updateGUIRecording();
 		this.timer_recording = new Timer(true);
 		this.timer_recording.schedule(new RecordingCounterTask(), 1000, 1000);
-		startRecordingAudio();
+		// startRecordingAudio();
+		startRecordingAudio2();
 	}
 
-	private void startRecordingAudio() {
-		// TODO Auto-generated method stub
-		Toast t1 = Toast.makeText(getApplicationContext(), "1",
-				Toast.LENGTH_SHORT);
-		Toast t2 = Toast.makeText(getApplicationContext(), "2",
-				Toast.LENGTH_SHORT);
-		Toast t3 = Toast.makeText(getApplicationContext(), "3",
-				Toast.LENGTH_SHORT);
-		t1.show();
-		mRecorder = new MediaRecorder();
-		t2.show();
-		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		t3.show();
-		mRecorder.setOutputFile(AUDIOFILENAME);
-		mRecorder.setAudioSamplingRate(44100);
-		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-		mRecorder.setMaxFileSize(0);
+	private void startRecordingAudio2() {
+		recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+				RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+				RECORDER_AUDIO_ENCODING, bufferSize);
 
-		try {
-			mRecorder.prepare();
-		} catch (IOException e) {
-			Log.e("HCI", "prepare() failed");
+		int i = recorder.getState();
+		if (i == 1)
+			recorder.startRecording();
+
+		recordingThread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				writeAudioDataToFile();
+			}
+		}, "AudioRecorder Thread");
+
+		recordingThread.start();
+
+	}
+
+	private String getTempFilename() {
+		String filepath = Environment.getExternalStorageDirectory().getPath();
+		File file = new File(filepath, Environment
+				.getExternalStorageDirectory().getAbsolutePath());
+
+		if (!file.exists()) {
+			file.mkdirs();
 		}
 
-		mRecorder.start();
-		// com.example.hci_sonar.PatterDetection.hasPattern("10010010");
+		File tempFile = new File(filepath, AUDIO_RECORDER_TEMP_FILE);
+
+		if (tempFile.exists())
+			tempFile.delete();
+
+		return (file.getAbsolutePath() + "/" + AUDIO_RECORDER_TEMP_FILE);
 	}
 
+	private void writeAudioDataToFile() {
+		byte data[] = new byte[bufferSize];
+		String filename = getTempFilename();
+		FileOutputStream os = null;
+
+		try {
+			os = new FileOutputStream(filename);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		int read = 0;
+
+		if (null != os) {
+			while (STATE == RECORDING) {
+				read = recorder.read(data, 0, bufferSize);
+
+				if (AudioRecord.ERROR_INVALID_OPERATION != read) {
+					try {
+						os.write(data);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			try {
+				os.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@SuppressWarnings("deprecation")
 	private void initVariables() {
 		// TODO Auto-generated method stub
 		this.STATE = IDLE;
 		this.seconds_recorded = 0;
 		this.seconds_sanction = 0;
+
+		bufferSize = AudioRecord.getMinBufferSize(8000,
+				AudioFormat.CHANNEL_CONFIGURATION_MONO,
+				AudioFormat.ENCODING_PCM_16BIT);
 
 	}
 
